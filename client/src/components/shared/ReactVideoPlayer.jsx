edf4c83e1a3170f316399cea61cedb86fcdda0e8
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-03-03 14:31:08 +0530

    Implemented the video player

--- client/src/components/shared/ReactVideoPlayer.jsx
+++ client/src/components/shared/ReactVideoPlayer.jsx
@@ -6,7 +6,7 @@ Returns the React Video Player component for videoplayback
 import ReactPlayer from "react-player";
 
 function ReactVideoPlayer({src}) {
-  return <ReactPlayer width="100%" height="100%" src={src}/>;
+  return <ReactPlayer width="100%" height="100%" src={src} controls playing={true}/>;
 }
 
 export default ReactVideoPlayer;
