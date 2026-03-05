b8a649f31ec5ec516c70a6a95a4777a827b00ac0
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-03-05 12:13:53 +0530

    Removed the asChild attribute from the accordion components

--- client/src/components/shared/Accordion.jsx
+++ client/src/components/shared/Accordion.jsx
@@ -33,14 +33,12 @@ export function CourseCommonAccordionItem({ children, value }) {
 // The accordion trigger
 export function CourseAccordionTrigger({ children }) {
   return (
-    <AccordionTrigger asChild>
-      <div className="w-full border-b rounded-none px-2 bg-white/3 border-white/5 text-md flex justify-center items-center">
-        {/* The trigger title */}
-        {children}
+    <AccordionTrigger className="w-full border-b rounded-none px-2 bg-white/3 border-white/5 text-md flex justify-between items-center">
+      {/* The trigger title */}
+      {children}
 
-        {/* The trigger icon */}
-        <ChevronDownIcon className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
-      </div>
+      {/* The trigger icon */}
+      <ChevronDownIcon className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
     </AccordionTrigger>
   );
 }
@@ -48,13 +46,11 @@ export function CourseAccordionTrigger({ children }) {
 // The accordion content
 export function CourseAccordionContent({ children }) {
   return (
-    <AccordionContent asChild>
+    <AccordionContent className="w-full p-3 flex flex-col gap-7 justify-between items-center">
       {/* The videos */}
-      <div className="w-full p-3 flex flex-col gap-7 justify-between items-center">
-        <ol className="w-full list-decimal p-3 pb-0 pl-7 flex flex-col justify-center items-start gap-2 text-lg">
-          {children}
-        </ol>
-      </div>
+      <ol className="w-full list-decimal p-3 pb-0 pl-7 flex flex-col justify-center items-start gap-2 text-lg">
+        {children}
+      </ol>
     </AccordionContent>
   );
 }
