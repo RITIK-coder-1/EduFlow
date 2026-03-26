5810da43a8d511a3ba5a5ee778abcd3afc11b892
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-03-26 17:00:26 +0530

    Updated the account type fetching script on the dashboard component

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -3,17 +3,17 @@ Dashboard.jsx
 The user dashboard
 ------------------------------------------------------------------------------------------------- */
 
-import { useSelector } from "react-redux";
 import StudentDashboard from "./StudentDashboard";
 import InstructorDashboard from "./InstructorDashboard";
 import AdminDashboard from "./AdminDashboard";
+import useUserStatus from "@/hooks/useUserStatus";
 
 function Dashboard() {
-  const user = useSelector((state) => state.auth.user);
+  const { accountType } = useUserStatus();
 
-  if (user?.accountType === "Student") return <StudentDashboard />;
-  else if (user?.accountType === "Instructor") return <InstructorDashboard />;
-  else if (user?.accountType === "Admin") return <AdminDashboard />;
+  if (accountType === "Student") return <StudentDashboard />;
+  else if (accountType === "Instructor") return <InstructorDashboard />;
+  else if (accountType === "Admin") return <AdminDashboard />;
 }
 
 export default Dashboard;
