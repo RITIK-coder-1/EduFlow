7a40c60bbde44eff6e79253eb1c1b0f727cc8b2a
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-07-16 13:14:17 +0530

    refactored the apiError class to TS

--- server/src/utils/api/apiError.ts
+++ server/src/utils/api/apiError.ts
@@ -1,14 +1,18 @@
 /* ---------------------------------------------------------------------------------------
-apiError.js
+apiError.ts
 This is a class to send specific API error objects on failure 
 ------------------------------------------------------------------------------------------ */
 
 class ApiError extends Error {
+  statusCode: number;
+  success: boolean;
+  errors: unknown[];
+
   constructor(
-    statusCode,
-    message = "API ERROR MESSAGE: Something went wrong!",
-    errors = [],
-    stack = ""
+    statusCode: number,
+    message: string = "API ERROR MESSAGE: Something went wrong!",
+    errors: unknown[] = [],
+    stack: string = ""
   ) {
     super(message);
     this.statusCode = statusCode;
