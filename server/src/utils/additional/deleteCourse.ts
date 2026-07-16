d61a1e876c4ab6b235812308b9c85a948a3c0b73
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-07-16 13:04:27 +0530

    refactored the entire delete course utility to TS

--- server/src/utils/additional/deleteCourse.ts
+++ server/src/utils/additional/deleteCourse.ts
@@ -12,9 +12,11 @@ import {
   CourseProgress,
   CourseSectionContract,
   CourseVideoContract,
+  UserContract,
 } from "../../models/index.model.ts";
 import { deleteFromCloudinary } from "../index.utils.ts";
 import ApiError from "../api/apiError.js";
+import { DeleteResult, UpdateResult } from "mongoose";
 
 const deleteCourse = async (courseId: string): Promise<void> => {
   // Getting the course
@@ -36,27 +38,30 @@ const deleteCourse = async (courseId: string): Promise<void> => {
   }
 
   // Deleting all the sections
-  const sectionDelete = CourseSection.deleteMany({ course: courseId });
+  const sectionDelete: Promise<DeleteResult> = CourseSection.deleteMany({
+    course: courseId,
+  });
 
   // Deleting all the videos
-  const videosDelete = course?.sections?.map((section) =>
-    CourseVideo.deleteMany({ courseSection: section._id })
-  );
+  const videosDelete: Promise<DeleteResult>[] =
+    course?.sections?.map((section) =>
+      CourseVideo.deleteMany({ courseSection: section._id })
+    ) || [];
 
   // Removing the course from the users' enrolled courses list
-  const userCourseDelete = User.updateMany(
+  const userCourseDelete: Promise<UpdateResult> = User.updateMany(
     { enrolledCourses: courseId },
     { $pull: { enrolledCourses: courseId } }
   );
 
   // Removing the course from the last course visited list of the users
-  const removeLastVisited = User.updateMany(
+  const removeLastVisited: Promise<UpdateResult> = User.updateMany(
     { lastCourseVisited: courseId },
     { $set: { lastCourseVisited: null } }
   );
 
   // Removing the course from the category lists
-  const categoryCourseDelete = CourseCategory.updateOne(
+  const categoryCourseDelete: Promise<UpdateResult> = CourseCategory.updateOne(
     { name: course?.category },
     {
       $pull: { courses: courseId },
@@ -64,14 +69,17 @@ const deleteCourse = async (courseId: string): Promise<void> => {
   );
 
   // Removing the course from the instructor's list
-  const instructorCourseDelete = User.findByIdAndUpdate(course?.owner, {
-    $pull: { createdCourses: courseId },
-  });
+  const instructorCourseDelete: Promise<UserContract | null> =
+    User.findByIdAndUpdate(course?.owner, {
+      $pull: { createdCourses: courseId },
+    });
 
   // deleting the course progress document of all the users
-  const courseProgressDelete = CourseProgress.deleteMany({ course: courseId });
+  const courseProgressDelete: Promise<DeleteResult> = CourseProgress.deleteMany(
+    { course: courseId }
+  );
 
-  const dbCleanUp = Promise.all([
+  const dbCleanUp: Promise<any> = Promise.all([
     sectionDelete,
     userCourseDelete,
     removeLastVisited,
@@ -87,11 +95,20 @@ const deleteCourse = async (courseId: string): Promise<void> => {
   const thumbnailDelete = deleteFromCloudinary(course?.thumbnail);
 
   // Deleting the videos from cloudinary
+
+  type MinimalVideo = Pick<CourseVideoContract, "videoUrl">; // Creating a new type using ONLY the 'videoUrl' field from CourseVideoContract
+
+  type MinimalSection = Pick<CourseSectionContract, "courseVideos"> & {
+    courseVideos: MinimalVideo[];
+  }; // Creating a new type using ONLY the 'courseVideos' field from CourseSectionContract
+
   // course object has sections array. Sections array has documents. Each sectionDocument has courseVideos array. The array has video documents
-  const videosDeleteCloudinary = course?.sections.map((sectionDocument) =>
-    sectionDocument.courseVideos.map((video) => {
-      deleteFromCloudinary(video?.videoUrl, "video");
-    })
+  const videosDeleteCloudinary = (
+    (course?.sections as unknown as MinimalSection[]) || []
+  ).flatMap((section) =>
+    section.courseVideos.map((video) =>
+      deleteFromCloudinary(video.videoUrl, "video")
+    )
   );
 
   const cloudinaryCleanUp = Promise.all([
@@ -123,8 +140,10 @@ const deleteCourse = async (courseId: string): Promise<void> => {
     await cloudinaryCleanUp.catch((error) => {
       console.error("COURSE DELETE ERROR: cloudinary", error);
     });
-  } catch (error) {
-    console.error("DELETE COURSE ERROR:", error);
+  } catch (error: unknown) {
+    error instanceof Error
+      ? console.error("DELETE COURSE ERROR:", error.message)
+      : console.error("DELETE COURSE ERROR:", error);
     throw new ApiError(500, "Failed to delete the course");
   }
 };
--- server/src/utils/files/deleteFromCloudinary.ts
+++ server/src/utils/files/deleteFromCloudinary.ts
@@ -16,7 +16,7 @@ cloudinary.config({
 type CloudinaryResourceType = "image" | "video" | "raw" | "auto";
 
 const deleteFromCloudinary = async (
-  fileUrl: string,
+  fileUrl: string | undefined,
   resource: CloudinaryResourceType = "image"
 ): Promise<void> => {
   if (!fileUrl) {
@@ -25,8 +25,8 @@ const deleteFromCloudinary = async (
 
   try {
     // firstly, I have to extract the public id out of the file url
-    const subStrings: string[] = fileUrl.split("/"); // an array of split url strings and the last string is the public id
-    const lastString: string | undefined = subStrings.pop(); // the public id
+    const subStrings: string[] | undefined = fileUrl?.split("/"); // an array of split url strings and the last string is the public id
+    const lastString: string | undefined = subStrings?.pop(); // the public id
     const publicId: string | undefined = lastString?.split(".")[0]; // we need the public id without the extension
 
     if (publicId) {
