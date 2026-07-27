5ee52dfbe3db8535fb07d9a9cfc2f9eef9c95166
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-07-27 13:37:32 +0530

    added a response contract to the queryResponses utility

--- client/src/utils/queryResponses.ts
+++ client/src/utils/queryResponses.ts
@@ -1,11 +1,16 @@
 /* ----------------------------------------------------------------------------------------------
-queryResponses.js
+queryResponses.ts
 This function provides the specific transformed responses for Redux Toolkit Query success and errors
 ------------------------------------------------------------------------------------------------- */
 
+interface ResponseContract {
+  data?: unknown;
+  message: string;
+}
+
 function queryResponses() {
   // success response
-  const transformResponse = (response) => {
+  const transformResponse = (response): ResponseContract => {
     return {
       data: response?.data,
       message: response?.message,
@@ -13,12 +18,10 @@ function queryResponses() {
   };
 
   // error response
-  const transformErrorResponse = (response) => {
-    return (
-      {
-        message: response?.data?.message,
-      } || { message: "An error occurred" }
-    );
+  const transformErrorResponse = (response): ResponseContract => {
+    return {
+      message: response?.data?.message,
+    };
   };
 
   return { transformResponse, transformErrorResponse };
