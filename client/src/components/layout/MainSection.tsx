e1a6642c86cfb7f2bb954404ec5d288fc8811991
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-07-28 16:28:15 +0530

    refactored MainSection component to TSX

--- client/src/components/layout/MainSection.tsx
+++ client/src/components/layout/MainSection.tsx
@@ -1,9 +1,15 @@
 /* ---------------------------------------------------------------------------------------
-MainSection.jsx
+MainSection.tsx
 The main section of the app that containst the main body content of each page 
 ------------------------------------------------------------------------------------------ */
 
-function MainSection({ children }) {
+import { ReactNode } from "react";
+
+interface MainSectionProps {
+  children: ReactNode;
+}
+
+function MainSection({ children }: MainSectionProps) {
   return (
     <main className="w-full min-h-screen flex flex-col justify-start items-center gap-5 pt-3 relative">
       {children}
