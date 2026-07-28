27876cd174b17727a495ec6474cd628f5e546637
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-07-28 11:54:56 +0530

    Fixed my courseApi type syntax: gave proper types to the builder and query methods. Added response transformers.

--- client/src/api/courses/courseApi.ts
+++ client/src/api/courses/courseApi.ts
@@ -3,7 +3,16 @@ courseApi.ts
 This file does all the course related PUBLIC API calls 
 ------------------------------------------------------------------------------------------------- */
 
+import {
+  ResponseContract,
+  CourseContract,
+  CourseCategoryContract,
+} from "../../types/index.types.ts";
 import apiSlice from "../base/apiSlice.ts";
+import {
+  transformResponse,
+  transformErrorResponse,
+} from "../../utils/queryResponses.ts";
 
 interface MinimalCourse {
   courseId: string;
@@ -12,30 +21,41 @@ interface MinimalCourse {
 const courseApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     // GET ALL THE COURSES
-    getAllTheCourses: builder.query({
+    getAllTheCourses: builder.query<ResponseContract<CourseContract[]>, void>({
       query: () => "/courses",
       providesTags: ["Course"],
+      transformResponse: transformResponse<CourseContract[]>(),
+      transformErrorResponse,
     }),
 
     // GET A PARTICULAR COURSE
-    getCourse: builder.query({
-      query: ({ courseId }: MinimalCourse) => `/courses/${courseId}`,
+    getCourse: builder.query<ResponseContract<CourseContract>, MinimalCourse>({
+      query: ({ courseId }) => `/courses/${courseId}`,
       providesTags: ["Course"],
+      transformResponse: transformResponse<CourseContract>(),
+      transformErrorResponse,
     }),
 
     // ENROLL INTO A COURSE
-    enrollCourse: builder.mutation({
-      query: ({ courseId }: MinimalCourse) => ({
+    enrollCourse: builder.mutation<ResponseContract<null>, MinimalCourse>({
+      query: ({ courseId }) => ({
         url: `/courses/${courseId}`,
         method: "POST",
       }),
       invalidatesTags: ["Course", "User"],
+      transformResponse: transformResponse<null>(),
+      transformErrorResponse,
     }),
 
     // SHOW ALL CATEGORIES
-    getAllCategories: builder.query({
+    getAllCategories: builder.query<
+      ResponseContract<CourseCategoryContract[]>,
+      void
+    >({
       query: () => "/courses/categories",
       providesTags: ["Category"],
+      transformResponse: transformResponse<CourseCategoryContract[]>(),
+      transformErrorResponse,
     }),
   }),
 });
