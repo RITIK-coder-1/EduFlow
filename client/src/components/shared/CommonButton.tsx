77a05396aa81cdde292f36277d4e334f2dfaa72e
Ritik Mahapatra <ritikprofessional111@gmail.com>
2026-08-01 13:53:37 +0530

    refactored CommonButton to TSX

--- client/src/components/shared/CommonButton.tsx
+++ client/src/components/shared/CommonButton.tsx
@@ -3,20 +3,34 @@ Button.jsx
 ------------------------------------------------------------------------------------------ */
 
 import { Button } from "@/components/ui/button";
+import React from "react";
+
+type ButtonType = "button" | "submit" | "reset" | undefined;
+
+interface CommonButtonContract {
+  type?: ButtonType;
+  label: string;
+  className?: string;
+  title?: string;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  disabled?: boolean;
+}
 
 function CommonButton({
   type = "button",
   label,
   onClick = () => {},
   className,
-  title
-}) {
+  title,
+  disabled,
+}: CommonButtonContract) {
   return (
     <Button
       type={type}
       className={`text-lg w-50 p-5 shadow-2xl shadow-black hover:bg-purple-950 border border-black/90 ${className}`}
       onClick={onClick}
       title={title}
+      disabled={disabled}
     >
       {label}
     </Button>
